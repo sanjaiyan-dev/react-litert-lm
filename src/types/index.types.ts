ea295d731c3f9e56ba1ae1ff611bbee18e5adae5
Sanjaiyan Parthipan <parthipankalayini@gmail.com>
2026-06-06 21:33:26 +0530

    Add TypeScript types and documentation for LiteRtEngine and chat message props
    - Created index.types.ts to export various types including LiteRtEngineProviderProps and chat-related props.
    - Added HTML documentation for UseLiteRtChatNonStreamSendMessageProps and UseLiteRtChatStreamSendMessageProps.
    - Introduced types_index.types.html for module documentation.
    - Established LiteRtEngineContext variable documentation.
    - Configured TypeDoc with entry points and output settings in typedoc.json.

---
+++ src/types/index.types.ts
@@ -0,0 +1,17 @@
+import { LiteRtEngineProviderProps } from "./base/LiteRtEngineProvider.types";
+import {
+	UseLiteRtChatConversationInitProps,
+	UseLiteRtChatNonStreamProps,
+	UseLiteRtChatNonStreamSendMessageProps,
+	UseLiteRtChatStreamProps,
+	UseLiteRtChatStreamSendMessageProps,
+} from "./base/useLiteRtChat.types";
+
+export {
+	LiteRtEngineProviderProps,
+	UseLiteRtChatConversationInitProps,
+	UseLiteRtChatNonStreamProps,
+	UseLiteRtChatNonStreamSendMessageProps,
+	UseLiteRtChatStreamProps,
+	UseLiteRtChatStreamSendMessageProps,
+};
